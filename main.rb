6d726f508a714e3b486664581c5c2073d60d58ae
kypaz <abouquet@riseup.net>
2017-11-24 15:10:00 +0100

    init

---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative 'parser'
+
+id = "4257485149504554485151485049"
+res = get_pb_athlete(id)
+puts(res)
---
+++ parser.rb
@@ -0,0 +1,33 @@
+require 'open-uri'
+require 'nokogiri'
+require 'watir'
+require 'json'
+
+def get_pb_athlete(id)
+  base_url = "http://bases.athle.com/asp.net/athletes.aspx?"
+  record = "base=records&seq="
+  url = base_url+record+id
+
+  browser = Watir::Browser.new
+  browser.goto url
+  doc = Nokogiri::HTML.parse(browser.html)
+
+  res = []
+
+  i = 2
+  while doc.css(".linedRed table tr:nth-of-type("+i.to_s+")").text != ""
+    row = {}
+    event = doc.css(".linedRed table tr:nth-of-type("+i.to_s+") td[1]").text
+    row["event"] = event
+    result = doc.css(".linedRed table tr:nth-of-type("+i.to_s+") td[2]").text
+    row["result"] = result
+    location = doc.css(".linedRed table tr:nth-of-type("+i.to_s+") td[6]").text
+    row["location"] = location
+    date = doc.css(".linedRed table tr:nth-of-type("+i.to_s+") td[7]").text
+    row["date"] = date
+    row["show"] = true
+    res.push(row)
+    i = i +1
+  end
+  return res.to_json
+end
